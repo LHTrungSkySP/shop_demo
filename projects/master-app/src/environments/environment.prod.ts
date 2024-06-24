4f92f51c99104edb37511e55dc253a13614269f8
Trung Lê Hoàng <trunglh@pmr.vn>
2024-06-24 17:46:28 +0700

    deploy

--- projects/master-app/src/environments/environment.prod.ts
+++ projects/master-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  shopping: 'https://minh-trang-do-gom.vercel.app/shopping/remoteEntry.js',
+  shopping: 'https://shop-shopping.vercel.app/remoteEntry.js',
   manage: 'https://minh-trang-do-gom.vercel.app/manage/remoteEntry.js'
 };
